191a76e72ada20291539feb0d80acb6fc1f67ed7
jose orozco <orozco.jose.7378@eam.edu.co>
2025-08-20 09:23:39 -0500

    ADD CORRECTIONS

---
+++ app/src/main/java/EjerciciosKotlin/Ejercicio2/DiagramaURL/URL.kt
@@ -0,0 +1,9 @@
+package EjerciciosKotlin.Ejercicio2.DiagramaURL
+
+class URL {
+    var urlDiagrama = "https://miro.com/welcomeonboard/ZjZLYW8yalNhcUJGSjNYcUU0eEJUODJnWlVvZkNqaUl6TmNVS3V1dG43OFNqWkh1RVZmbEpXaklrTmUwWnhacXNHRXIxaTlLRkRYQWlRckFNMnRSMm5odWVjanFRYjdZWFBGUVVxMWdyTlNmaDRuaFZoalBrQW5xT3owNVV5dUlBS2NFMDFkcUNFSnM0d3FEN050ekl3PT0hdjE=?share_link_id=716676268653"
+
+    /*
+    Profe no supe donde mas poner el enlace
+    * */
+}
